6f6cd8c5ca5c14fe709e570fcd6f56549b5de212
sikumel-ux <sikumel@gmail.com>
2026-01-20 21:09:05 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,22 @@
-self.addEventListener('fetch', function(event) {
-    // Service Worker Minimalis
+const CACHE_NAME = 'mahika-trans-v1';
+// Cukup daftarkan file HTML dan Ikon saja
+const assets = [
+  'pwa-dash.html', // Sesuaikan nama file dashboardmu
+  'shuttle-bus.png'       // Sesuaikan nama file ikonmu
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return res || fetch(event.request);
+    })
+  );
 });
